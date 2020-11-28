db1ff47d5e3f93e8c95b9e8356ce47b8afebddbf
Shikata Ga Nai <nickbhe@gmail.com>
2020-11-28 18:49:20 +0200

    Add project 4 and builds

---
+++ #4 Stealth/Assets/Scripts/GameUI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject gameLoseScreen;
+    public GameObject gameWinScreen;
+    public Text pointsText;
+    public Text gameModeText;
+    public Button gameModeButton;
+    public bool isHardModeOn;
+    bool gameStopped;
+
+    void Start() {
+        PointPlatform.OnGainPoint += OnGainPoint;
+        gameLoseScreen.SetActive(false);
+        gameWinScreen.SetActive(false);
+        Guard.OnPlayerSpotted += OnGameLose;
+        PointPlatform.OnPlayerWin += OnGameWin;
+        ToggleGameMode(true);
+        gameModeButton.onClick.AddListener(() => ToggleGameMode(!isHardModeOn));
+    }
+
+    void Update() {
+        if (gameStopped) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    void ToggleGameMode(bool isHardMode) {
+        isHardModeOn = isHardMode;
+        if (isHardModeOn) {
+            gameModeText.text = "Hard";
+            gameModeText.color = Color.red;
+        } else {
+            gameModeText.text = "Easy";
+            gameModeText.color = Color.green;
+        }
+    }
+
+    void OnGainPoint(PointPlatform p) {
+        pointsText.text = PointPlatform.points.ToString();
+    }
+
+    void OnGameWin() {
+        OnGameOver(gameWinScreen);
+    }
+
+    void OnGameLose() {
+        if (isHardModeOn) {
+            OnGameOver(gameLoseScreen);
+        }
+    }
+
+    void OnGameOver(GameObject gameOverUI) {
+        gameOverUI.SetActive(true);
+        gameStopped = true;
+        Guard.OnPlayerSpotted -= OnGameLose;
+        PointPlatform.OnPlayerWin -= OnGameWin;
+        PointPlatform.OnGainPoint -= OnGainPoint;
+    }
+}
---
+++ #4 Stealth/Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float moveSpeed = 7;
+	public float smoothMoveTime = .1f;
+	public float turnSpeed = 8;
+
+	float angle;
+	float smoothInputMagnitude;
+	float smoothMoveVelocity;
+	public Vector3 velocity;
+	public bool isColliding;
+	bool disabled;
+	Vector2 savepoint;
+
+	new Rigidbody rigidbody;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+		PointPlatform.OnPlayerWin += Disable;
+		PointPlatform.OnGainPoint += UpdateSavepoint;
+		Guard.OnPlayerSpotted += Spotted;
+	}
+
+	void Update() {
+		// Receive player input or disable movement
+		Vector3 inputDirection = Vector3.zero;
+		if (!disabled) {
+			inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+		}
+		
+		// Calculate multiplier to smooth velocity
+		float inputMagnitude = inputDirection.magnitude;
+		smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+		// Calculate interpolated value
+		float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+		angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+		velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+	}
+
+	void FixedUpdate() {
+		// Move and rotate the rigidBody over fixed time so calculations are correct
+		rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+		rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+	}
+
+	void UpdateSavepoint(PointPlatform pointPlatform) {
+		savepoint = new Vector2(pointPlatform.transform.position.x, pointPlatform.transform.position.z);
+	}
+
+	void Spotted() {
+		if(GameObject.FindObjectOfType<GameUI>().isHardModeOn) {
+			Disable();
+		} else {
+			ReturnToSavepoint();
+		}
+    }
+
+	void Disable() {
+		disabled = true;
+    }
+
+	void ReturnToSavepoint() {
+		transform.position = new Vector3(savepoint.x, transform.position.y, savepoint.y);
+    }
+
+	private void OnTriggerEnter(Collider other) {
+		savepoint = new Vector2(other.transform.position.x, other.transform.position.z);
+	}
+
+
+	void OnDestroy() {
+		PointPlatform.OnPlayerWin -= Disable;
+		PointPlatform.OnGainPoint -= UpdateSavepoint;
+		Guard.OnPlayerSpotted -= Spotted;
+	}
+}
