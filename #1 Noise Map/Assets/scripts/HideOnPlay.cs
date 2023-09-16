58325b9041b534d6008defbb0e81831487f24145
Shikata Ga Nai <nickbhe@gmail.com>
2023-09-16 08:44:06 +0300

    zip files

--- #1 Noise Map/Assets/scripts/HideOnPlay.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HideOnPlay : MonoBehaviour {
-    void Start() {
-        gameObject.SetActive(false);
-    }
-}
